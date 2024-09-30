c12c94929ac72fb2cdf4fb4d5de6ab3fa9a54fef
kennethviov <151752526+kennethviov@users.noreply.github.com>
2024-09-30 05:48:00 -0700

    Add files via upload

---
+++ Exception/Displays.java
@@ -0,0 +1,48 @@
+  public class Displays {
+
+    // Menus
+    static void menu() {
+      System.out.println("\nMain Menu");
+      System.out.println("[1] Encapsulation");
+      System.out.println("[2] Inheritance");
+      System.out.println("[3] Polymorphism");
+      System.out.println("[4] Abstraction");
+      System.out.println("[0] Exit");
+    }
+    
+    static void encapmenu() {
+      System.out.println("\nEncapsulation Menu: Rectangle Attributes");
+      System.out.println("[1] Set Rectangle Attributes");
+      System.out.println("[2] Display Attributes");
+      System.out.println("[3] Calculate Area");
+      System.out.println("[4] Calculate Perimeter");
+      System.out.println("[0] Back to Main Menu");
+    }
+    
+    static void inhermenu() {
+      System.out.println("\nInheritance Menu: Shape Area Calculator");
+      System.out.println("[1] Set Circle Radius");
+      System.out.println("[2] Set Square Side");
+      System.out.println("[3] Calculate Circle Area");
+      System.out.println("[4] Calculate Square Area");
+      System.out.println("[0] Back to Main Menu");
+    }
+
+    static void polymenu() {
+      System.out.println("\nPolymorphism Menu: Payment Processing");
+      System.out.println("[1] Credit Card Payment");
+      System.out.println("[2] PayPal Payment");
+      System.out.println("[0] Back to Main Menu");
+    }
+
+    static void abstmenu() {
+      System.out.println("\nAbstraction Menu: Appliance Operation");
+      System.out.println("[1] Power On Refrigerator");
+      System.out.println("[2] Power Off Refrigerator");
+      System.out.println("[3] Power On Washing Machine");
+      System.out.println("[4] Power Off Washing Machine");
+      System.out.println("[0] Back to Main Menu");
+    }
+
+    // 
+  }
