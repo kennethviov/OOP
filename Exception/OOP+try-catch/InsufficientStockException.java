b99f43436aab85b442b1d2099bbfca61c9565e4d
kennethviov <151752526+kennethviov@users.noreply.github.com>
2024-10-02 22:05:39 +0800

    Update InsufficientStockException.java

--- Exception/OOP+try-catch/InsufficientStockException.java
+++ Exception/OOP+try-catch/InsufficientStockException.java
@@ -1,3 +1,11 @@
 class InsufficientStockException extends RuntimeException {
-   
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
+
+class InvalidQuantityException extends RuntimeException {
+    public InvalidQuantityException(String message) {
+        super(message);
+    }
 }
