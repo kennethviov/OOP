1cb1aaee58762bda5d753e2f122afc7b7b4faabf
kennethviov <151752526+kennethviov@users.noreply.github.com>
2024-10-02 19:27:36 -0700

    Update Inventory.java

--- Exception/OOP+try-catch/Inventory.java
+++ Exception/OOP+try-catch/Inventory.java
@@ -37,4 +37,13 @@ class Inventory implements InventoryOperation {
                 "\nQuantity: "     + product.getQuantity());
         }
     }
+    
+    public Product getProduct(String productId) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getProductId().equals(productId)) {
+                return list.get(i);
+            }
+        }
+        throw new ProductNotFoundException("Product not found.");
+    }
 }
